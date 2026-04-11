1aa53b3358acc76ccea852f7bbdc1fcc2829b72f
Nethsara Shyamika <nethsarashyamika@gmail.com>
2026-04-11 14:04:58 +0530

    Update Jest setup and remove safety checks

--- backend/tests/setup.js
+++ backend/tests/setup.js
@@ -1,4 +1,3 @@
+import dotenv from "dotenv";
+dotenv.config();
 process.env.NODE_ENV = "test";
-
-process.env.MONGO_URI =
-  "mongodb+srv://aquashield_app:AquaShield2026@aquashieldcluster.jua3ahm.mongodb.net/testdb";
